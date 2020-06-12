3dca439573cb385b87648c3f21bbd9c11ab36b71
ПК <89068414983@bk.ru>
2020-06-12 22:09:41 +0500

    first

--- 15.py
+++ 15.py
@@ -1,47 +1,34 @@
-import telebot
-from telebot import apihelper
+import requests
+import time
+import pprint
+
+TOKEN = '1065481689:AAHijHJaohcanNZqZvYka0caReD9S44xPDw'
+
+BOT_URL = f'https://api.telegram.org/bot{TOKEN}'
 
 
-Token = '1101589848:AAEfRJ_ML0fouwFYFnd1-NFcBQ6jv7JzeDs'
-
-Bot_URL = f'https://api.telegram.org/bot{Token}'
 
 proxies = {
-    'http': 'http://88.199.21.76:80',
-    'https': 'http://88.199.21.76:80'
+    'http': 'http://51.158.98.121:8811',
+    'https': 'http://51.158.98.121:8811',
 }
 
-apihelper.proxy = proxies
-bot = telebot.TeleBot(Token)
+url = f'{BOT_URL}/getMe'
+result = requests.get(url, proxies = proxies)
 
-@bot.message_handler(command = ['start'])
-def command_answer(message):
-    bot.reply_to(message, f'Рад Вас приветствовать,{message.from_user.username}!')
 
-@bot.message_handler(command = ['help'])
-def command_help(message):
-    bot.reply_to(message, 'Обратитесь за помощью к администратору!')
-
-@bot.message_handler(command = ['quit'])
-def command_help(message):
-    bot.reply_to(message, 'Хотите выйти? Примените формулу прощания!')
-
-#инициация прощания
-@bot.message_handler(content_types = ['text'])
-def farewell(message):
-    if message.text == 'До свидания!':
-        bot.reply_to(message, f'До связи,{message.from_user.username}!')
-
-#отсылка к команде "/help"
-@bot.message_handler(content_types = ['text'])
-def receive(message):
-    if '?' in message.text:
-        bot.reply_to(message, f'Вы написали: {text}. Если у вас вопрос, наберите команду /help')
-
-#ответ на вопрос "Кто ты?"
-@bot.message_handler(content_types=['text'])
-def bot_horoscope(message):
-    if message.text == 'Кто ты?':
-        bot.reply_to(message, 'Я твой всезнающий мистер Бот, тебе повезло!')
-
-bot.polling()
+#print(result.status_code)
+url = f'{BOT_URL}/getUpdates'
+while True:
+    time.sleep(3)
+    result = requests.get(url, proxies=proxies)
+    pprint.pprint(result.json())
+    messages = result.json()['result']
+    for message in messages:
+        chat_id = message['message']['chat']['id']
+        url_send = f'{BOT_URL}/sendMessage'
+        params = {
+                'chat_id': chat_id,
+                'text': 'Добрый день!'
+                  }
+        answer = requests.post(url_send, params = params, proxies = proxies)
